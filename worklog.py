792b2552aaededb8e046699abf9c97102c8eebcd
Chris H <crhowell1@gmail.com>
2016-08-04 17:29:36 -0700

    Add initial functions with notes

---
+++ worklog.py
@@ -0,0 +1,54 @@
+import csv
+from datetime import datetime
+
+
+def create_file():
+    # Create a new csv file on local drive
+    pass
+
+
+def open_file():
+    # Try to open existing csv file
+    # if exists, load into dict return it
+    # if it doesnt exist, create_file()
+    # then open and return as empty dict
+    pass
+
+
+def print_delete_menu():
+    # Display menu options related to Delete
+    pass
+
+
+def print_edit_menu():
+    # Display menu options related to Edit
+    pass
+
+
+def print_find_menu():
+    # Display menu options related to Find
+    pass
+
+
+def print_add_menu():
+    # Display menu options related to Add
+    pass
+
+
+def print_menu():
+    # Display Main selection menu
+    # Add, Find, Edit, Delete
+    pass
+
+
+def main():
+    # Print Main Menu
+    # Ask for Input
+    # Validate Input
+    # Do selected option... 
+    # Loop until user 'quits'
+    return False
+
+
+if __name__ == '__main__':
+    main()
